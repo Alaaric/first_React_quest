e6170b694b5a57ba7a54cb678a76e5455781419a
Alaric HENROT <alaric.henrot@gmail.com>
2023-03-28 11:50:19 +0200

    change function name and add pokemon list

--- quest2/src/components/PokemonCard.jsx
+++ quest2/src/components/PokemonCard.jsx
@@ -1,11 +1,26 @@
 
-function Bulbasaur() {
+
+function PokemonCard() {
+
+    let Mokepon = pokemonList[1];
+
   return (
     <figure>
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"></img>
-        <figcaption>bulbasaur</figcaption>
+        {Mokepon.hasOwnProperty("imgSrc") ? <img src={Mokepon.imgSrc}></img> : <p>???</p>}
+        <figcaption>{Mokepon.name}</figcaption>
     </figure>
   );
 }
 
-export default Bulbasaur;
+const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
+
+export default PokemonCard;
